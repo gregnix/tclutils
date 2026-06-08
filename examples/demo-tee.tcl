effe05d46af57a82f268f2a6921d7f92318d0c46
gregnix <gregor.ebbing@gmx.de>
2026-06-08 06:31:24 +0200

    update

--- examples/demo-tee.tcl
+++ examples/demo-tee.tcl
@@ -1,7 +1,7 @@
 source [file join [file dirname [file normalize [info script]]] bootstrap.tcl]
 package require tclutils::tutee
 
-set a [file join $root demo-tee-a.txt]
-set b [file join $root demo-tee-b.txt]
+set a [file join $root  examples  demo-tee-a.txt]
+set b [file join $root examples  demo-tee-b.txt]
 ::tclutils::tutee::write "hello from tutee" [list $a $b]
 puts "wrote $a and $b"
